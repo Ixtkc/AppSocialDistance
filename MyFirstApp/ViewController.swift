f484f2770a3f644f772f515a89244bce8c62ca91
Takashi <nakano.takashi@amail.plala.or.jp>
2020-04-21 20:25:47 +0900

    first app

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Takashi Nakano on 2020/04/21.
+//  Copyright © 2020 Takashi Nakano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
