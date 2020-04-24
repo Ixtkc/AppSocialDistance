f75913dbb6aa2b3b9dd5367e038a54f31c9f8715
Takashi <nakano.takashi@amail.plala.or.jp>
2020-04-25 00:04:15 +0900

    First commit

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -5,17 +5,40 @@
 //  Created by Takashi Nakano on 2020/04/21.
 //  Copyright © 2020 Takashi Nakano. All rights reserved.
 //
+//
+//  ボタンを押すと pheripheral の名前を取得し、配列に追加. その際の配列はユニークな値であるように保持.
+//
+//
+//
+//
+//
+//
 
 import UIKit
+import CoreBluetooth
+
+
+extension Array where Element: Hashable {
+
+    func addUnique(array: Array) -> Array {
+        let dict = Dictionary(map{ ($0, 1) }, uniquingKeysWith: +)
+        return self + array.filter{ dict[$0] == nil }
+    }
+}
+
 
 class ViewController: UIViewController {
+    
+    var uuids = Array<UUID>()
+    var names = [UUID : String]()
+    var peripherals = [UUID : CBPeripheral]()
+    var centralManager: CBCentralManager!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         outputLabel.text = "Stay Home."
-        
         
     }
 
@@ -24,5 +47,32 @@ class ViewController: UIViewController {
     @IBAction func shuffleButton(_ sender: Any) {
         outputLabel.text = "Hung on."
     }
+    
+    /// PheripheralのScanが成功したら呼び出される。
+    ///
+    /// - Parameters:
+    ///   - central: <#central description#>
+    ///   - peripheral: <#peripheral description#>
+    ///   - advertisementData: <#advertisementData description#>
+    ///   - RSSI: <#RSSI description#>
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral,
+                        advertisementData: [String: Any], rssi RSSI: NSNumber) {
+        print("pheripheral.name: \(String(describing: peripheral.name))")
+        print("advertisementData:\(advertisementData)")
+        print("RSSI: \(RSSI)")
+        print("peripheral.identifier.uuidString: \(peripheral.identifier.uuidString)")
+        let uuid = UUID(uuid: peripheral.identifier.uuid)
+        self.uuids.append(uuid)
+        let kCBAdvDataLocalName = advertisementData["kCBAdvDataLocalName"] as? String
+        if let name = kCBAdvDataLocalName {
+            self.names[uuid] = name.description
+        } else {
+            self.names[uuid] = "no name"
+        }
+        self.peripherals[uuid] = peripheral
+    }
+    
 }
+
+
 
