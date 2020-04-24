a35b1ef1d35041b771b9975572477a9bbf010680
Takashi <nakano.takashi@amail.plala.or.jp>
2020-04-25 02:01:18 +0900

    still shut down immedietly

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -28,15 +28,33 @@ class ViewController: UIViewController {
     var peripherals = [UUID : CBPeripheral]()
     var centralManager: CBCentralManager!
     var array_master: [String] = []
+    let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
         outputLabel.text = String(array_master.count)
+        
+        // サイズ
+        button.frame = CGRect(x: 0, y: 0, width: 200, height: 40)
+        button.backgroundColor = UIColor.darkGray
+        button.layer.masksToBounds = true
+        button.setTitle("Search", for: UIControl.State.normal)
+        button.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        button.layer.cornerRadius = 20.0
+        button.layer.position = CGPoint(x: self.view.frame.width/2, y:self.view.frame.height-50)
+        button.tag = 1
+        button.addTarget(self, action: #selector(onClickMyButton(sender:)), for: .touchUpInside)
+        
+        // UIボタンをViewに追加.
+        self.view.addSubview(button);
     }
 
     @IBOutlet weak var outputLabel: UILabel!
-    @IBAction func shuffleButton(_ sender: Any) {
+    
+    /// ボタンが押されたときに呼び出される。
+    @objc func onClickMyButton(sender: UIButton){
         // CoreBluetoothを初期化および始動.
         centralManager = CBCentralManager(delegate: self, queue: nil, options: nil)
     }
@@ -45,6 +63,26 @@ class ViewController: UIViewController {
 
 extension ViewController: CBCentralManagerDelegate{
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        print("state \(central.state)")
+
+        switch central.state {
+        case .poweredOff:
+            print("Bluetoothの電源がOff")
+        case .poweredOn:
+            print("Bluetoothの電源はOn")
+            // BLEデバイスの検出を開始.
+            centralManager.scanForPeripherals(withServices: nil)
+        case .resetting:
+            print("レスティング状態")
+        case .unauthorized:
+            print("非認証状態")
+        case .unknown:
+            print("不明")
+        case .unsupported:
+            print("非対応")
+        @unknown default:
+            print("?")
+        }
     }
 
     /// PheripheralのScanが成功したら呼び出される。
